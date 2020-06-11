a3413c470e2a76354c0cb7ce4a69fa7ce3a6cb0d
Jummit <jummit@web.de>
2020-06-11 11:49:58 +0200

    add support for multiple networking instances

--- addons/replicator/RemoteSpawner.gd
+++ addons/replicator/RemoteSpawner.gd
@@ -6,19 +6,22 @@ func _ready():
 
 # node names are replicated in `spawn()`,
 # but there is no way to include @s in custom names
-func _on_SceneTree_node_added(node):
+func _on_SceneTree_node_added(node : Node):
 	node.name = node.name.replace("@", "")
 
 
 remote func spawn(node_name : String, network_master : int, scene_path : String, path : NodePath) -> void:
-	print("Spawned instance of %s with the name of %s as child of %s" % [scene_path, node_name, path])
+	print("Spawned instance of %s with the name of %s as child of %s on peer %s" % [scene_path, node_name, path, multiplayer.get_network_unique_id()])
 	var instance : Node = load(scene_path).instance()
 	instance.name = node_name
 	instance.set_network_master(network_master)
-	get_node(path).add_child(instance)
+	
+	# use a path relative to multiplayer.root_node to make it possible
+	# to run server and client on the same machine
+	multiplayer.root_node.get_node(path).add_child(instance)
 
-	# hide the instance because its position may not yet
-	# be replicated to avoid seeing the instance at 0, 0
+	# hide the instance as its position may not yet be
+	# replicated to avoid seeing the instance at the origin
 	if instance.has_method("show") and instance.has_method("hide"):
 		instance.hide()
 		yield(get_tree().create_timer(.01), "timeout")
--- addons/replicator/Replicator.gd
+++ addons/replicator/Replicator.gd
@@ -35,47 +35,48 @@ export var logging := false
 
 const TYPES_WITH_EQUAL_APPROX_METHOD := [TYPE_VECTOR2, TYPE_RECT2, TYPE_VECTOR3, TYPE_TRANSFORM2D, TYPE_PLANE, TYPE_QUAT, TYPE_AABB, TYPE_BASIS, TYPE_TRANSFORM, TYPE_COLOR]
 
+var remote_spawner : Node
 var already_replicated_once : Dictionary = {}
 var last_replicated_values : Dictionary = {}
 
 onready var subject : Node = get_parent()
 
 func _ready():
-	for member in members_to_replicate.split("\n", false):
-		var tween := Tween.new()
-		tween.name = member
-		add_child(tween)
-	if is_network_master() and spawn_on_joining_peers and not subject.filename.empty():
-		get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
-	if not is_network_master() and despawn_on_disconnect:
-		get_tree().connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
-	if is_network_master() and replicate_automatically:
-		var timer := Timer.new()
-		timer.wait_time = replicate_interval
-		timer.autostart = true
-		timer.one_shot = false
-		timer.connect("timeout", self, "_on_ReplicateTimer_timeout")
-		add_child(timer)
+	if is_network_master():
+		remote_spawner = find_node_on_parents(self, "RemoteSpawner")
+
+		if spawn_on_joining_peers and not subject.filename.empty():
+			multiplayer.connect("network_peer_connected", self, "_on_network_peer_connected")
+		if replicate_automatically:
+			var timer := Timer.new()
+			timer.wait_time = replicate_interval
+			timer.autostart = true
+			timer.one_shot = false
+			timer.connect("timeout", self, "_on_ReplicateTimer_timeout")
+			add_child(timer)
+		if replicate_despawning:
+			connect("tree_exiting", self, "_on_tree_exiting")
+
+		if replicate_spawning:
+			remote_spawner.rpc("spawn", get_parent().name, get_network_master(), get_parent().filename, multiplayer.root_node.get_path_to(get_parent().get_parent()))
+			yield(get_tree(), "idle_frame")
+			replicate_members()
+	else:
+		if despawn_on_disconnect:
+			multiplayer.connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
+		for member in members_to_replicate.split("\n", false):
+			var tween := Tween.new()
+			tween.name = member
+			add_child(tween)
 
 
-func _notification(what):
-	if not (is_inside_tree() and is_network_master()):
-		return
-	
-	match what:
-		NOTIFICATION_ENTER_TREE:
-			if replicate_spawning:
-				RemoteSpawner.rpc("spawn", get_parent().name, get_network_master(), get_parent().filename, get_parent().get_parent().get_path())
-				yield(get_tree(), "idle_frame")
-				replicate_members()
-		NOTIFICATION_EXIT_TREE:
-			if replicate_despawning:
-				rpc("remove")
+func _on_tree_exiting():
+	rpc("remove")
 
 
 func _on_network_peer_connected(id):
 	_log("Spawned %s on newly connected peer %s" % [subject.filename, id])
-	RemoteSpawner.rpc_id(id, "spawn", subject.name, get_network_master(), subject.filename, subject.get_parent().get_path())
+	remote_spawner.rpc_id(id, "spawn", get_parent().name, get_network_master(), get_parent().filename, multiplayer.root_node.get_path_to(get_parent().get_parent()))
 
 
 func _on_network_peer_disconnected(id):
@@ -90,10 +91,13 @@ func _on_ReplicateTimer_timeout():
 
 
 func replicate_members(reliable := false) -> void:
+	if members_to_replicate.empty():
+		return
 	for member in members_to_replicate.split("\n"):
 		var last_value = last_replicated_values.get(member)
 		var current_value = subject.get(member)
 		if not _is_equal_approx(current_value, last_value):
+			assert(get(member) == null)
 			callv("rpc" if reliable else "rpc_unreliable", ["replicate_member", member, current_value])
 			last_replicated_values[member] = current_value
 
@@ -118,8 +122,19 @@ func _log(message : String) -> void:
 		print(message)
 
 
-func _is_equal_approx(a, b) -> bool:
+static func _is_equal_approx(a, b) -> bool:
 	if a == null or b == null:
 		return false
 	else:
 		return a.is_equal_approx(b) if typeof(a) in TYPES_WITH_EQUAL_APPROX_METHOD else a == b
+
+
+static func find_node_on_parents(start_node : Node, node_name : String):
+	if not start_node.get_parent():
+		return false
+
+	var node := start_node.get_parent().find_node(node_name)
+	if node:
+		return node
+	else:
+		return find_node_on_parents(start_node.get_parent())
