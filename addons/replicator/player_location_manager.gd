66ab79ba8a1e58c35839f4664a4585f78a060a10
Jummit <jummit@web.de>
2021-01-12 15:36:20 +0100

    make player cameras optional

--- addons/replicator/player_location_manager.gd
+++ addons/replicator/player_location_manager.gd
@@ -16,29 +16,33 @@ func _ready() -> void:
 
 
 func register(player_camera : Node) -> void:
-	rpc("_register_player_camera", multiplayer.root_node.get_path_to(player_camera))
+	rpc("_register_player_camera", player_camera)
 
 
 func get_distance(from_node : Node, to_id : int) -> float:
-	var to : Node = player_cameras[to_id]
-	if not to:
+	if not to_id in player_cameras:
 		return -1.0
-	if from_node is Spatial and to is Spatial:
-		return from_node.global_transform.origin.distance_to(to.global_transform.origin)
-	elif from_node is Node2D and to is Node2D:
-		return from_node.global_position.distance_to(to.global_position)
+	var camera = player_cameras[to_id]
+	if from_node is Spatial and camera is Spatial:
+		return from_node.global_transform.origin.distance_to(
+				camera.global_transform.origin)
+	elif from_node is Node2D and camera is Node2D:
+		return from_node.global_position.distance_to(camera.global_position)
 	return -1.0
 
 
 func _on_network_peer_connected(id : int) -> void:
 	if id == multiplayer.get_network_unique_id():
 		return
-	var my_camera : Node = player_cameras[multiplayer.get_network_unique_id()]
-	if my_camera:
-		rpc_id(id, "_register_player_camera", multiplayer.root_node.get_path_to(my_camera))
+	if multiplayer.get_network_unique_id() in player_cameras:
+		rpc_id(id, "_register_player_camera",
+				player_cameras[multiplayer.get_network_unique_id()])
 
 
 remotesync func _register_player_camera(player_camera_path : NodePath) -> void:
-	if enable_logging and not multiplayer.get_network_unique_id() == multiplayer.get_rpc_sender_id():
-		print("Registering %s as camera of peer %s" % [player_camera_path, multiplayer.get_rpc_sender_id()])
-	player_cameras[multiplayer.get_rpc_sender_id()] = multiplayer.root_node.get_node(player_camera_path)
+	if enable_logging and not multiplayer.get_network_unique_id() ==\
+			multiplayer.get_rpc_sender_id():
+		print("Registering %s as camera of peer %s" % [player_camera_path,
+				multiplayer.get_rpc_sender_id()])
+	player_cameras[multiplayer.get_rpc_sender_id()] =\
+			multiplayer.root_node.get_node(player_camera_path)
