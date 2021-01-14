3520148b2e357ee1cd1fddbff93f55b41d210f65
Jummit <jummit@web.de>
2021-01-14 12:51:07 +0100

    make player location manager server-only

--- addons/replicator/player_location_manager.gd
+++ addons/replicator/player_location_manager.gd
@@ -11,38 +11,27 @@ export var enable_logging := false
 
 var player_cameras : Dictionary = {}
 
-func _ready() -> void:
-	multiplayer.connect("network_peer_connected", self, "_on_network_peer_connected")
-
-
-func register(player_camera : Node) -> void:
-	rpc("_register_player_camera", player_camera)
+func register(camera : Node, peer : int) -> void:
+	player_cameras[peer] = camera.get_path()
+	if enable_logging:
+		print("Registered %s as camera of peer %s" % [camera.get_path(), peer])
 
 
 func get_distance(from_node : Node, to_id : int) -> float:
 	if not to_id in player_cameras:
+		if enable_logging:
+			print("No camera found for %s" % to_id)
 		return -1.0
-	var camera = player_cameras[to_id]
+	var camera = multiplayer.root_node.get_node(player_cameras[to_id])
+	var distance := -1.0
 	if from_node is Spatial and camera is Spatial:
-		return from_node.global_transform.origin.distance_to(
+		distance = from_node.global_transform.origin.distance_to(
 				camera.global_transform.origin)
 	elif from_node is Node2D and camera is Node2D:
-		return from_node.global_position.distance_to(camera.global_position)
-	return -1.0
-
-
-func _on_network_peer_connected(id : int) -> void:
-	if id == multiplayer.get_network_unique_id():
-		return
-	if multiplayer.get_network_unique_id() in player_cameras:
-		rpc_id(id, "_register_player_camera",
-				player_cameras[multiplayer.get_network_unique_id()])
-
-
-remotesync func _register_player_camera(player_camera_path : NodePath) -> void:
-	if enable_logging and not multiplayer.get_network_unique_id() ==\
-			multiplayer.get_rpc_sender_id():
-		print("Registering %s as camera of peer %s" % [player_camera_path,
-				multiplayer.get_rpc_sender_id()])
-	player_cameras[multiplayer.get_rpc_sender_id()] =\
-			multiplayer.root_node.get_node(player_camera_path)
+		distance = from_node.global_position.distance_to(camera.global_position)
+	else:
+		push_error("Types of %s (type %s) and camera %s (type %s) don't match, can't get distance"\
+				% [from_node, from_node.get_class(), camera, camera.get_class()])
+	if enable_logging:
+		print("Distance from %s to %s is %s" % [from_node, to_id, distance])
+	return distance
