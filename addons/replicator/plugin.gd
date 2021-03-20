0ec365bfc3489a54ba244cd78989434202a6ac7b
Jummit <jummit@web.de>
2021-03-20 12:48:22 +0100

    use local paths

--- addons/replicator/plugin.gd
+++ addons/replicator/plugin.gd
@@ -2,12 +2,12 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("Replicator", "Node", load("res://addons/replicator/replicator.gd"), preload("replicator_node_icon.svg"))
-	add_custom_type("RemoteSpawner", "Node", load("res://addons/replicator/remote_spawner.gd"), preload("replicator_node_icon.svg"))
-	add_custom_type("PlayerLocationManager", "Node", load("res://addons/replicator/player_location_manager.gd"), preload("replicator_node_icon.svg"))
+	add_custom_type("Replicator", "Node", load("replicator.gd"), preload("replicator_node_icon.svg"))
+	add_custom_type("RemoteSpawner", "Node", load("remote_spawner.gd"), preload("replicator_node_icon.svg"))
+	add_custom_type("PlayerLocationManager", "Node", load("player_location_manager.gd"), preload("replicator_node_icon.svg"))
 	
-	add_autoload_singleton("RemoteSpawner", "res://addons/replicator/remote_spawner.gd")
-	add_autoload_singleton("PlayerLocationManager", "res://addons/replicator/player_location_manager.gd")
+	add_autoload_singleton("RemoteSpawner", "remote_spawner.gd")
+	add_autoload_singleton("PlayerLocationManager", "player_location_manager.gd")
 
 
 func _exit_tree():
